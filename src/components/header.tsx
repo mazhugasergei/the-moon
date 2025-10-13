b636093572be3fa5703cac6364476e12fb737744
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-10-13 14:35:14 +1000

    fix: home link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,25 @@
 "use client"
 
-import Link from "next/link"
-import { usePathname } from "next/navigation"
+import { useEffect, useState } from "react"
 
 export function Header() {
-	const pathname = usePathname()
+	const [mounted, setMounted] = useState(false)
+	const [root, setRoot] = useState("/")
 
-	// remove everything after the first "/" in the path
-	const rootPath = pathname.split("/")[1] ? "/" : "/"
+	useEffect(() => {
+		setRoot(window.location.href.split("/").slice(0, 3).join("/"))
+		setMounted(true)
+	}, [])
 
 	return (
 		<header className="text-white/50">
-			<Link href={rootPath} className="fixed top-0 left-0 z-50 p-4">
-				Home
-			</Link>
+			{mounted ? (
+				<a href={root} className="fixed top-0 left-0 z-50 p-4">
+					Home
+				</a>
+			) : (
+				<span className="fixed top-0 left-0 z-50 p-4">Home</span>
+			)}
 
 			<div className="fixed top-0 left-1/2 z-50 -translate-x-1/2 p-4">(Moon)</div>
 		</header>
