28fbcce5dee88c323165c8010ecf3762a9a6e503
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-10-13 14:19:32 +1000

    fix: home link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,18 @@
+"use client"
+
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export function Header() {
+	const pathname = usePathname()
+
+	// remove everything after the first "/" in the path
+	const rootPath = pathname.split("/")[1] ? "/" : "/"
+
 	return (
 		<header className="text-white/50">
-			<Link href="/" className="fixed top-0 left-0 z-50 p-4">
-				Back
+			<Link href={rootPath} className="fixed top-0 left-0 z-50 p-4">
+				Home
 			</Link>
 
 			<div className="fixed top-0 left-1/2 z-50 -translate-x-1/2 p-4">(Moon)</div>
